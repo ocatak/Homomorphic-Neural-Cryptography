d9f3e1176ca3b9701257350a2eff1e89508c8cab
minawoien <mc.woien@stud.uis.no>
2024-02-15 13:25:04 +0100

    testing HO_model

--- test_alice.py
+++ test_alice.py
@@ -2,7 +2,7 @@ from networks import alice, p1_bits, p2_bits
 import numpy as np
 from EllipticCurve import generate_key_pair
 from keras.models import Model
-from keras.layers import Input, Reshape
+from keras.layers import Input, Reshape, concatenate
 from keras.optimizers import RMSprop
 from keras.callbacks import ModelCheckpoint
 import numpy as np
@@ -15,39 +15,49 @@ from nac import NAC
 from data_utils import generate_static_dataset
 
 # Check if weights folder exists
-# This folder store the trained model weights
+# This folder store the trained HO_model weights
 if not os.path.exists('weights'):
     os.makedirs('weights')
 
 
+c1_bits = 572
+c2_bits = 572 
+
+c3_bits = 572
+
 # Hyper parameters used for training
 units = 2
 batch_size = 5
-num_samples = 864
+num_samples = 572
 # num_samples = 864*batch_size #rows
 
 # Create a task function for addition
 task_name = 'addition'
 task_fn = lambda x, y: x + y
 
-# Generate the model with an input layer and two NAC layers
-ip = Input(shape=(864,2,))
-x = NAC(units)(ip)
+# Generate the HO_model network with an input layer and two NAC layers
+units = 2
+# ip = Input(shape=(c3_bits, 2,)) # Define 2 inputs of size c1_bits
+HOinput1 = Input(shape=(c1_bits))  # ciphertext 1
+HOinput2 = Input(shape=(c2_bits))  # ciphertext 2
+
+HO_reshape1 = Reshape((c1_bits, 1))(HOinput1)
+HO_reshape2 = Reshape((c2_bits, 1))(HOinput2)
+
+HOinput =  concatenate([HO_reshape1, HO_reshape2], axis=-1)
+x = NAC(units)(HOinput)
 x = NAC(1)(x)
-x = Reshape((864,))(x)
+x = Reshape((c3_bits,))(x)
 
-model = Model(ip, x)
+HO_model = Model(inputs=[HOinput1, HOinput2], outputs=x)
 
-model_out = model([ip])
-print(model_out)
-
-# Compile the model
+# Compile the HO_model
 # Use RMSprop as the optimizer and mean squared error as the loss function
 optimizer = RMSprop(0.1)
-model.compile(optimizer, 'mse')
+HO_model.compile(optimizer, 'mse')
 
 # Generate training and testing datasets
-X_train, y_train = generate_static_dataset(task_fn, num_samples, mode='interpolation')
+X1_train, X2_train, y_train = generate_static_dataset(task_fn, num_samples, batch_size, mode='interpolation')
 
 private_arr, public_arr = generate_key_pair(batch_size)
 
@@ -55,40 +65,32 @@ p1_batch = np.random.randint(0, 2, p1_bits * batch_size).reshape(batch_size, p1_
 p2_batch = np.random.randint(0, 2, p2_bits * batch_size).reshape(batch_size, p2_bits)
 cipher1, cipher2 = alice.predict([public_arr, p1_batch, p2_batch])
 
-X_test = []
 y_test = []
+assert callable(task_fn)
 for i in range(len(cipher1)):
-    X_bit_array = []
-    y_bit_array = []
-    for j in range(len(cipher1[i])):
-        X_bit_array.append([cipher1[i][j], cipher2[i][j]])
-        y_bit_array.append([cipher1[i][j] + cipher2[i][j]])
-    X_test.append(X_bit_array)
-    y_test.append(y_bit_array)
-X_test = np.array(X_test)
+    Y = task_fn(cipher1[i], cipher2[i])
+    y_test.append(Y)
+
 y_test = np.array(y_test)
-print(X_test.shape)
 
 
-# X_test, y_test = generate_static_dataset(task_fn, num_samples, mode='extrapolation')
-
-# Use 'ModelCheckpoint' callback to save the model weights during training, specifically saving only the best-performing weights based on validation loss
+# Use 'ModelCheckpoint' callback to save the HO_model weights during training, specifically saving only the best-performing weights based on validation loss
 weights_path = 'weights/%s_weights.h5' % (task_name)
 checkpoint = ModelCheckpoint(weights_path, monitor='val_loss',
                              verbose=1, save_weights_only=True, save_best_only=True)
 
 callbacks = [checkpoint]
 
-# Train model and includes validation using the test dataset
-model.fit(X_train, y_train, batch_size=64, epochs=500,
-          verbose=2, callbacks=callbacks, validation_data=(X_test, y_test))
+# Train HO_model and includes validation using the test dataset
+HO_model.fit([X1_train, X2_train], y_train, batch_size=64, epochs=500,
+          verbose=2, callbacks=callbacks, validation_data=([cipher1, cipher2], y_test))
 
-# Evaluate the model on the test data set and the mean squared error of the model on the test dataset is printed
-model.load_weights(weights_path)
+# Evaluate the HO_model on the test data set and the mean squared error of the HO_model on the test dataset is printed
+HO_model.load_weights(weights_path)
 
-scores = model.evaluate(X_test, y_test, batch_size=128)
-predicted = model.predict(X_test, 128)
-print(X_test[:3])
+scores = HO_model.evaluate([cipher1, cipher2], y_test, batch_size=128)
+predicted = HO_model.predict([cipher1, cipher2], 128)
+print([cipher1, cipher2][:3])
 print(y_test[:3])
 print(predicted[:3])
 print(predicted.shape) #!Bob and Eve's shape
