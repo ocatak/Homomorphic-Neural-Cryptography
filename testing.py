352e2697c4e589bb836a51061bbd76fb283159ea
minawoien <mc.woien@stud.uis.no>
2024-02-20 15:27:45 +0100

    updated testing saved weights

--- testing.py
+++ testing.py
@@ -1,22 +1,18 @@
-import numpy as np
+from networks import HO_model, c3_bits
+from data_utils import generate_static_dataset
 
-# batch_size = 1000
-batch_size = 1000
-p1_bits = 8
-p2_bits = 8
+task_fn = lambda x, y: x + y
+num_samples = c3_bits
+batch_size = 512
 
-p1_batch = np.random.randint(0, 2, p1_bits * batch_size).reshape(batch_size, p1_bits)
-p2_batch = np.random.randint(0, 2, p2_bits * batch_size).reshape(batch_size, p2_bits)
+weights_path = 'weights/addition_weights.h5'
 
-X_train = []
-y_train = []
-for i in range(len(p1_batch)):
-    p1_binary_string = ''.join(str(bit) for bit in p1_batch[i])
-    p2_binary_string = ''.join(str(bit) for bit in p2_batch[i])
-    
-    X_train.append([ int(p1_binary_string, 2), int(p2_binary_string, 2)])
-    y_train.append([ int(p1_binary_string, 2) + int(p2_binary_string, 2)])
+HO_model.load_weights(weights_path)
 
-print(X_train)
-print(y_train)
 
+X1, X2, y = generate_static_dataset(task_fn, num_samples, batch_size)
+
+predicted = HO_model.predict([X1, X2], 128)
+
+print(predicted)
+print(y)
