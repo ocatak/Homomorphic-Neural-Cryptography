b866e8e43cc580656f9a71f759de7475f25abedd
minawoien <mc.woien@stud.uis.no>
2024-03-08 16:15:23 +0100

    added ciphertext for secp224r1 with rate 03

--- results.py
+++ results.py
@@ -28,7 +28,7 @@ for curve in curves:
     nonce = np.random.rand(batch_size, nonce_bits)
     for rate in dropout_rates:
         results[curve][rate] = {}
-        alice, bob, HO_model, eve, _ = create_networks(public_arr.shape[1], private_arr.shape[1], rate)
+        alice, bob, HO_model, eve, _, _, _, _, _, _, _, _ = create_networks(public_arr.shape[1], private_arr.shape[1], rate)
         path_name = f"rate-{rate}-curve-{curve}"
         weights_path = f'weights/weights-{path_name}'
         if not os.path.exists(weights_path):
