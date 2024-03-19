f033ae98e8b370872b10d5b65e3fda27ee5122b5
minawoien <mc.woien@stud.uis.no>
2024-03-19 11:08:51 +0100

    adding init files for folder structure

--- data_utils/average_loss.py
+++ data_utils/average_loss.py
@@ -9,7 +9,7 @@ loss = {'ABloss': 0, 'Bobloss': 0, 'Eveloss': 0}
 
 # Sum the last loss value of each file
 for i in range(1, num_files + 1):
-    df = pd.read_csv(f'{curve}/{cycles}cycle/test-{i}.csv')
+    df = pd.read_csv(f'dataset/multiplication-addition-rate-0-HO-loss.csv')
     for j in loss.keys():
         y_ab = df[j][len(df[j])-1]
         loss[j] += y_ab
