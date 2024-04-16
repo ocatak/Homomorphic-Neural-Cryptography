d04c6c67d1fe6530cbf7efd170e2c8114ba773a7
minawoien <mc.woien@stud.uis.no>
2024-04-16 20:06:47 +0200

    move addition figures to arkiv

--- data_utils/analyse_cipher.py
+++ data_utils/analyse_cipher.py
@@ -26,40 +26,32 @@ def probabilistic_encryption_analysis(rate: float, curve: str) -> Tuple[float, f
     std = np.std(stacked_arrays, axis=0)
     return std[0]
 
-def plot_std_and_mean(curve):
+def plot_std_and_mean(curve: str):
     """Plots the mean and standard deviation of the standard deviation of five ciphertexts for a given curve. 
     Saves the plot as a pdf file.
 
     Args:
         curve: Elliptic curve.
     """
-    # Initialize Seaborn theme
     sns.set_theme(style="darkgrid")
 
-    # Define the dropout rates
     dropout_rates = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
-    # Initialize lists to store the data
     data = []
-
-    # Loop through the dropout rates, call the function, and append data
     for rate in dropout_rates:
         std = probabilistic_encryption_analysis(rate, curve)
-        # Append standard deviation values
         for s in std:
             data.append({'value': s, 'type': 'mean of std', 'dropout_rate': rate})
 
-    # Create a DataFrame from the data list
     df = pd.DataFrame(data)
 
-    # Plotting
     plt.figure(figsize=(10, 6))
-    plt.rc('font', size=12)  # controls default text size
-    plt.rc('axes', titlesize=16)  # fontsize of the axes title
-    plt.rc('axes', labelsize=25)  # fontsize of the x and y labels
-    plt.rc('xtick', labelsize=25)  # fontsize of the tick labels
-    plt.rc('ytick', labelsize=25)  # fontsize of the tick labels
-    plt.rc('legend', fontsize=18)  # fontsize of the legend
+    plt.rc('font', size=12)  
+    plt.rc('axes', titlesize=16)  
+    plt.rc('axes', labelsize=25) 
+    plt.rc('xtick', labelsize=25)  
+    plt.rc('ytick', labelsize=25)  
+    plt.rc('legend', fontsize=18)  
     plt.ylim(0, 0.3)
     sns.lineplot(data=df, x='dropout_rate', y='value', hue='type', style='type', markers=True, dashes=False, errorbar='sd')
     plt.xlabel('Dropout Rate')
